f37a1fa58786a4e187e12d255a4b0edad7974da3
Andrés López Atehortua <anderson17colombia@hotmail.com>
2021-05-05 23:23:23 +0200

    ADDING login

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,3 +1,15 @@
+import { fetchWithoutToken } from '../../helpers/fetch';
+import { authTypes } from '../types/authTypes';
+
 export const startLoginAction = (email, password) => async (dispatch) => {
-  console.log(email, password);
+  const resp = await fetchWithoutToken('auth', { email, password }, 'POST');
+  const body = await resp.json();
+
+  if (body.success) {
+    localStorage.setItem('token', body.token);
+    localStorage.setItem('token-init-date', new Date().getTime());
+    dispatch(loginAction({ uid: body.uid, name: body.name }));
+  }
 };
+
+const loginAction = (user) => ({ type: authTypes.LOGIN, payload: user });
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,9 +1,13 @@
+import { authTypes } from '../types/authTypes';
+
 const initialState = {
   checking: true,
 };
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
+    case authTypes.LOGIN:
+      return { ...state, checking: false, ...action.payload };
     default:
       return state;
   }
